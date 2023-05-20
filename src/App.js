7cbb612f168e32aa5369e1f012a31ec51dd9b497
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2023-05-19 21:33:03 -0400

    Chp 3_4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import UploadForm from './components/UploadForm';
 import './App.css';
 
 const photos = [
@@ -12,16 +14,21 @@ const photos = [
 ]
 
 function App() {
+  const [items, setItems] = useState(photos);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const toggle = () => setIsCollapsed(!isCollapsed);
   return (
     <>
     <Navbar />
-    
-
     <div className="container text-center mt-5">
+      <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
+      <div className="clearfix mb-4"></div>
+      <UploadForm isVisible={isCollapsed} />
       <h1>Gallery</h1>
       <div className="row">
         {
-          photos.map((photo) => <Card src={photo} /> )
+          items.map((photo) => <Card src={photo} /> )
         }
       </div>
     </div>
