7682369516f40d32fd4f5bf381806bcde5dbd807
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2023-05-22 21:53:26 -0400

    Chp 4_4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 import UploadForm from './components/UploadForm';
@@ -14,18 +14,40 @@ const photos = [
 ]
 
 function App() {
+  const [count, setCount] = useState();
+  const [inputs, setInputs] = useState( { title: null, file: null, path: null });
   const [items, setItems] = useState(photos);
   const [isCollapsed, setIsCollapsed] = useState(false);
-
   const toggle = () => setIsCollapsed(!isCollapsed);
+  const handleOnChange = (e) => {
+    if (e.target.name === 'file') {
+      setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
+    } else {
+      setInputs({...inputs, title: e.target.value})
+    }
+  }
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setItems([inputs.path, ...items])
+  }
+
+  useEffect(() => {
+    setCount(`you have ${items.length} photo${items.length > 1 ? "s" : ''} in your gallery`)
+  }, [items])
+
   return (
     <>
     <Navbar />
     <div className="container text-center mt-5">
       <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
       <div className="clearfix mb-4"></div>
-      <UploadForm isVisible={isCollapsed} />
+      <UploadForm 
+        isVisible={isCollapsed} 
+        onChange={handleOnChange}
+        onSubmit={handleOnSubmit}
+      />
       <h1>Gallery</h1>
+      {count}
       <div className="row">
         {
           items.map((photo) => <Card src={photo} /> )
