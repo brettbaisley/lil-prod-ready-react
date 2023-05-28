12d10cf274e5374bfa008f2c4f1c5311b76f83a3
Brett Baisley <26395611+brettbaisley@users.noreply.github.com>
2023-05-27 22:11:51 -0400

    03_08: complete

--- src/App.js
+++ src/App.js
@@ -1,60 +1,83 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useReducer } from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
 import UploadForm from './components/UploadForm';
 import './App.css';
 
-const photos = [
-  'https://picsum.photos/id/1001/200/200',
-  'https://picsum.photos/id/1002/200/200',
-  'https://picsum.photos/id/1003/200/200',
-  'https://picsum.photos/id/1004/200/200',
-  'https://picsum.photos/id/1005/200/200',
-  'https://picsum.photos/id/1006/200/200',
-]
+const photos = []
+
+const initialState = {
+  items: photos,
+  count: photos.length,
+  inputs: { title: null, file: null, path: null },
+  isCollapsed: false
+}
+
+const handleOnChange = (state, e) => {
+  if (e.target.name === 'file') {
+    return { ...state.inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])}
+  } else {
+    return {...state.inputs, title: e.target.value}
+  }
+}
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case 'setItem':
+      return {
+        ...state,
+        items: [state.inputs, ...state.items]
+      }
+    case 'setInputs':
+      return {
+        ...state,
+        inputs: handleOnChange(state, action.payload.value)
+      }
+    case 'collapse':
+      return {
+        ...state,
+        isCollapsed: action.payload.bool
+      }
+      default : return state
+  }
+}
+
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState)
   const [count, setCount] = useState();
-  const [inputs, setInputs] = useState( { title: null, file: null, path: null });
-  const [items, setItems] = useState(photos);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-  const toggle = () => setIsCollapsed(!isCollapsed);
-  const handleOnChange = (e) => {
-    if (e.target.name === 'file') {
-      setInputs({ ...inputs, file: e.target.files[0], path: URL.createObjectURL(e.target.files[0])})
-    } else {
-      setInputs({...inputs, title: e.target.value})
-    }
-  }
+  const toggle = (bool) => dispatch({ type: 'collapse', payload: { bool }})
+  const handleOnChange = (e) => dispatch({ type: 'setInputs', payload: { value: e }})
   const handleOnSubmit = (e) => {
     e.preventDefault();
-    setItems([inputs.path, ...items])
-    setInputs({ title: null, file: null, path: null })
-    setIsCollapsed(false)
+    dispatch({ type: 'setItem' })
+    toggle(!state.isCollapsed)
   }
 
   useEffect(() => {
-    setCount(`you have ${items.length} photo${items.length > 1 ? "s" : ''} in your gallery`)
-  }, [items])
+    console.log(state)
+  }, [state.items])
+
+  useEffect(() => {
+    setCount(`you have ${state.items.length} photo${state.items.length > 1 ? "s" : ''} in your gallery`)
+  }, [state.items])
 
   return (
     <>
     <Navbar />
     <div className="container text-center mt-5">
-      <button className="btn btn-success float-end" onClick={toggle}>{isCollapsed ? 'Close' : '+ Add'}</button>
+      <button className="btn btn-success float-end" onClick={toggle}>{!state.isCollapsed ? 'Close' : '+ Add'}</button>
       <div className="clearfix mb-4"></div>
       <UploadForm 
-        inputs={inputs}
-        isVisible={isCollapsed} 
+        inputs={state.inputs}
+        isVisible={state.isCollapsed} 
         onChange={handleOnChange}
         onSubmit={handleOnSubmit}
       />
       <h1>Gallery</h1>
       {count}
       <div className="row">
-        {
-          items.map((photo) => <Card src={photo} /> )
-        }
+        { state.items.map((photo,index) => <Card key={index} src={photo.path} />) }
       </div>
     </div>
     </>
